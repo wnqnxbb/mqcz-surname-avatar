2138cb8048ee463bc5c1b63a7d60e2ef034a3e99
zhaomingxuan <mingxuanzhao@sohu-inc.com>
2024-11-12 20:31:22 +0800

    first commit

---
+++ src/main/java/com/mqcz/mqczsurnameavatar/MqczSurnameAvatarApplication.java
@@ -0,0 +1,13 @@
+package com.mqcz.mqczsurnameavatar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqczSurnameAvatarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqczSurnameAvatarApplication.class, args);
+    }
+
+}
